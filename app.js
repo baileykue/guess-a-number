0d60dcee9e47f13850c3d06077ef3907cb3fbb0c
Bailey Kuebelbeck <bailey.kue@gmail.com>
2021-10-04 15:26:19 -0700

    wrote the js function for guessing the number, includes high/low message for user, and guess counter

--- app.js
+++ app.js
@@ -1,8 +1,30 @@
-// import functions and grab DOM elements
+const button = document.getElementById('button');
+const input = document.getElementById('input');
+const userGuess = input.value;
+let userMessage = document.getElementById('user-message');
 
-// initialize global state
+let correctNum = Math.floor(Math.random()*(20-1)+1);
 
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+let guessesNum = 4;
+
+button.addEventListener('click', () => {
+  guessesNum--;
+  console.log(guessesNum);
+  console.log(correctNum);
+  Number(userGuess.value);
+  if (correctNum === userGuess) {
+    userMessage.textContent = 'You are correct! Well done!';
+  } else if (guessesNum === 0) {
+    userMessage.textContent = 'You have lost! No more guesses for you';
+  } else if (userGuess > correctNum) {
+    userMessage.textContent = `Your guess is too high! You have ${guessesNum} 
+    guesses left`;
+  } else if (userGuess < correctNum) {
+    userMessage.textContent = `Your guess is too low! you have ${guessesNum} 
+    guesses left`;
+  } 
+  /*else {
+    userMessage.textContent = 'Please Make sure you are entering a number between 1-20!';
+    guessesNum++;
+  }*/
+})
