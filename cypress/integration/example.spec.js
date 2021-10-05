e79f64306eb017c79522d281882cc80746dbb63d
Bailey Kuebelbeck <bailey.kue@gmail.com>
2021-10-05 11:00:28 -0700

    added some cypress testing

--- cypress/integration/example.spec.js
+++ cypress/integration/example.spec.js
@@ -1,4 +1,4 @@
-/*
+
 describe('Example Test', ()=>{
     before(()=>{
         cy.visit('/');
@@ -6,7 +6,15 @@ describe('Example Test', ()=>{
 
     describe('HTML elements', ()=>{   
         it('contains the alchemy logo', ()=>{
-            cy.get('.main-logo').should('be.visible');
+            cy.get('#button').should('be.visible');
         });
     });
-});*/
+    
+describe ('user interactions', =>(){
+    it('sumbits a users guess ad compares to the generated number', =>(){
+        cy.get('#button').click();
+        cy.get('input').should('be.visible');
+    })
+})
+
+});
