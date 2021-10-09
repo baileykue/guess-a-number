65bd99d1210dc3dfddd2f613b2973312879c7d2a
Violet Stewart <vkazoo2@gmail.com>
2021-10-08 17:23:47 -0700

    commit 3

--- test/example.test.js
+++ test/example.test.js
@@ -1,13 +1,12 @@
 // IMPORT MODULES under test here:
-// import { example } from '../example.js';
-
+//import { example } from '../example.js';
 const test = QUnit.test;
 
 test('time to test a function', (expect) => {
     //Arrange
     // Set up your arguments and expectations
     const expected = true;
-    
+
     //Act 
     // Call the function you're testing and set the result to a const
     const actual = true;
@@ -16,3 +15,4 @@ test('time to test a function', (expect) => {
     // Make assertions about what is expected versus the actual result
     expect.equal(actual, expected);
 });
+
